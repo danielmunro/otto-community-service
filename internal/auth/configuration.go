b36ae0939e7c9373a502053c72cd7c6dbc795e1b
Dan Munro <dan@danmunro.com>
2022-01-05 18:33:30 -0800

    add kustomization

--- internal/auth/configuration.go
+++ internal/auth/configuration.go
@@ -63,7 +63,7 @@ type Configuration struct {
 // NewConfiguration returns a new Configuration object
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
-		BasePath:      "http://" + os.Getenv("USER_SERVICE_HOST"),
+		BasePath:      "http://user-service",
 		DefaultHeader: make(map[string]string),
 		UserAgent:     "OpenAPI-Generator/1.0.0/go",
 	}
