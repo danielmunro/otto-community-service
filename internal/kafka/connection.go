7da4af42abf39c07e1a0e2319fa3973fa9248b67
Dan Munro <dan@danmunro.com>
2022-01-14 20:40:05 -0800

    use the right env var

--- internal/kafka/connection.go
+++ internal/kafka/connection.go
@@ -21,7 +21,7 @@ func GetReader() *kafka.Reader {
 	}
 
 	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:   []string{os.Getenv("KAFKA_BOOTSTRAP_SERVER")},
+		Brokers:   []string{os.Getenv("KAFKA_BOOTSTRAP_SERVERS")},
 		Topic:     string(constants.Users),
 		GroupID: "user_service",
 		Partition: 0,
