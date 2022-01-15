015c7d95a8ea0abeae4b95f1eccadb2d1b0593b3
Dan Munro <dan@danmunro.com>
2022-01-14 21:04:46 -0800

    try this

--- internal/kafka/connection.go
+++ internal/kafka/connection.go
@@ -4,22 +4,25 @@ import (
 	"github.com/danielmunro/otto-community-service/internal/constants"
 	"github.com/segmentio/kafka-go"
 	"github.com/segmentio/kafka-go/sasl/plain"
+	"github.com/segmentio/kafka-go/sasl/scram"
+	"log"
 	"os"
 	"time"
 )
 
 func GetReader() *kafka.Reader {
-	mechanism := plain.Mechanism{
-		Username: os.Getenv("KAFKA_SASL_USERNAME"),
-		Password: os.Getenv("KAFKA_SASL_PASSWORD"),
+	mechanism, err := scram.Mechanism(
+		scram.SHA512,
+		os.Getenv("KAFKA_SASL_USERNAME"),
+		os.Getenv("KAFKA_SASL_PASSWORD"))
+	if err != nil {
+		log.Panic(err)
 	}
-
 	dialer := &kafka.Dialer{
 		Timeout:       10 * time.Second,
 		DualStack:     true,
 		SASLMechanism: mechanism,
 	}
-
 	r := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:   []string{os.Getenv("KAFKA_BOOTSTRAP_SERVERS")},
 		Topic:     string(constants.Users),
