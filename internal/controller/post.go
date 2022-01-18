43c18d62997ca011f69b063423b544a1ed8e7a26
Dan Munro <dan@danmunro.com>
2022-01-17 20:03:51 -0800

    JFC

--- internal/controller/post.go
+++ internal/controller/post.go
@@ -72,7 +72,7 @@ func GetPostsV1(w http.ResponseWriter, r *http.Request) {
 	var viewerUuid uuid.UUID
 	if sessionToken != "" {
 		session, err := authService.GetSession(sessionToken)
-		if err != nil && session.User.Uuid != "" {
+		if err != nil && session != nil {
 			viewerUuid = uuid.MustParse(session.User.Uuid)
 		}
 	}
