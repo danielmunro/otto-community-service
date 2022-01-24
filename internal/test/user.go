11dbfaf380d7010837813ae01ded9d2a80f74481
Dan Munro <dan@danmunro.com>
2022-01-23 19:23:24 -0800

    randomize username in test users

--- internal/test/user.go
+++ internal/test/user.go
@@ -3,10 +3,14 @@ package test
 import (
 	"github.com/danielmunro/otto-community-service/internal/model"
 	"github.com/google/uuid"
+	"math/rand"
+	"strconv"
 )
 
 func CreateTestUser() *model.User {
+	randomInt := rand.Int()
 	return &model.User{
-		Uuid: uuid.New().String(),
+		Uuid:     uuid.New().String(),
+		Username: "user" + strconv.Itoa(randomInt),
 	}
 }
