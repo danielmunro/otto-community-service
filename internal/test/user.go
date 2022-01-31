0b7c4e8ffca89e0e1b99945eaa5eaf2d49e32adf
Dan Munro <dan@danmunro.com>
2022-01-30 17:49:25 -0800

    seed for randomness

--- internal/test/user.go
+++ internal/test/user.go
@@ -5,9 +5,11 @@ import (
 	"github.com/google/uuid"
 	"math/rand"
 	"strconv"
+	"time"
 )
 
 func CreateTestUser() *model.User {
+	rand.Seed(time.Now().UnixNano())
 	randomInt := rand.Int()
 	return &model.User{
 		Uuid:     uuid.New().String(),
