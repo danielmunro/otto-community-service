4f722e75e6160ac1f816a3e5bfa652e61a4a6895
Dan Munro <dan@danmunro.com>
2022-02-01 20:36:46 -0800

    wrong name

--- internal/repository/like.go
+++ internal/repository/like.go
@@ -19,7 +19,7 @@ func (l *LikeRepository) FindLikesForPosts(postUuids []uuid.UUID) []*entity.Post
 		"FROM post_likes " +
 		"WHERE post_id IN (SELECT id FROM posts WHERE uuid IN (?))"
 	var postLikes []*entity.PostLike
-	l.conn.Raw(query, postUuids).Preload("Posts").Scan(&postLikes)
+	l.conn.Raw(query, postUuids).Preload("Post").Scan(&postLikes)
 	return postLikes
 }
 
