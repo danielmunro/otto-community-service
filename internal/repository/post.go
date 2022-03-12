ecd52bf6cf8a02bddad94497095dbba91c654d96
Dan Munro <dan@danmunro.com>
2022-03-12 09:11:06 -0800

    filter out banned users

--- internal/repository/post.go
+++ internal/repository/post.go
@@ -26,7 +26,7 @@ func (p *PostRepository) FindByUser(user *entity.User, limit int) []*entity.Post
 		Preload("User").
 		Preload("Images").
 		Preload("SharePost").
-		Where("user_id = ? AND deleted_at IS NULL AND reply_to_post_id = 0", user.ID).
+		Where("user_id = ? AND users.is_banned = false AND deleted_at IS NULL AND reply_to_post_id = 0", user.ID).
 		Order("id desc").
 		Limit(limit).
 		Find(&posts)
@@ -41,7 +41,7 @@ func (p *PostRepository) FindByLikes(user *entity.User, limit int) []*entity.Pos
 		Preload("SharePost").
 		Table("posts").
 		Joins("join post_likes on post_likes.post_id = posts.id").
-		Where("post_likes.user_id = ? AND deleted_at IS NULL AND reply_to_post_id = 0", user.ID).
+		Where("post_likes.user_id = ? AND users.is_banned = false AND deleted_at IS NULL AND reply_to_post_id = 0", user.ID).
 		Order("id desc").
 		Limit(limit).
 		Find(&posts)
@@ -54,7 +54,7 @@ func (p *PostRepository) FindOneByUuid(uuid uuid.UUID) (*entity.Post, error) {
 		Preload("User").
 		Preload("Images").
 		Preload("SharePost").
-		Where("uuid = ? AND deleted_at IS NULL", uuid).
+		Where("uuid = ? AND users.is_banned = false AND deleted_at IS NULL", uuid).
 		Find(post)
 	if post.ID == 0 {
 		return nil, errors.New(constants.ErrorMessagePostNotFound)
@@ -71,7 +71,7 @@ func (p *PostRepository) FindByUserFollows(username string, limit int) []*entity
 		Table("posts").
 		Joins("join follows on follows.following_id = posts.user_id").
 		Joins("join users on follows.user_id = users.id").
-		Where("users.username = ? AND posts.deleted_at IS NULL AND posts.reply_to_post_id = 0", username).
+		Where("users.username = ? AND users.is_banned = false AND posts.deleted_at IS NULL AND posts.reply_to_post_id = 0", username).
 		Order("id desc").
 		Limit(limit).
 		Find(&posts)
@@ -86,7 +86,7 @@ func (p *PostRepository) FindAll(limit int) []*entity.Post {
 		Preload("SharePost").
 		Table("posts").
 		Joins("join users on posts.user_id = users.id").
-		Where("posts.deleted_at IS NULL AND users.deleted_at IS NULL AND posts.reply_to_post_id = 0").
+		Where("posts.deleted_at AND users.is_banned = false IS NULL AND users.deleted_at IS NULL AND posts.reply_to_post_id = 0").
 		Order("id desc").
 		Limit(limit).
 		Find(&posts)
@@ -101,7 +101,7 @@ func (p *PostRepository) FindByIDs(ids []uint) []*entity.Post {
 		Preload("SharePost").
 		Table("posts").
 		Joins("join users on posts.user_id = users.id").
-		Where("posts.deleted_at IS NULL AND posts.id IN (?)", ids).
+		Where("posts.deleted_at IS NULL AND users.is_banned = false AND posts.id IN (?)", ids).
 		Order("id desc").
 		Find(&posts)
 	return posts
